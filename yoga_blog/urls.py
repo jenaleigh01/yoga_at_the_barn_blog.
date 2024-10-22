b6c160082696d6f645386c1e1ce5d593f8780737
Jenna Guest <jenaleigh01@gmail.com>
2024-10-22 12:18:18 +0000

    created blog app

--- yoga_blog/urls.py
+++ yoga_blog/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import index
+from blog.views import my_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
+    path('blog', my_blog, name='blog'),
 ]
