ffb081ce4ab4c16a0e4eefbcdf5d81d7b0eee555
Jenna Guest <jenaleigh01@gmail.com>
2024-10-22 10:27:50 +0000

    created home app and test httpresponse view

--- yoga_blog/urls.py
+++ yoga_blog/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from home.views import index
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
