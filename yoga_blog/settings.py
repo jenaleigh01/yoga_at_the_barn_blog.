7e58f58207d3bc8037ca25a09364cf25c7c842d7
Jenna Guest <jenaleigh01@gmail.com>
2024-10-22 12:06:06 +0000

    database and secret key added

--- yoga_blog/settings.py
+++ yoga_blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--h-6*(+=wp*c8y=pk0f&emu!0v3d&^4tkt1!6x#f44&)4dj6zo'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
