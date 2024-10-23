6a870e01867da7ce0deb70220168f1f3437ba8e7
Jenna Guest <jenaleigh01@gmail.com>
2024-10-23 02:01:43 +0000

    created Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    # featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices= STATUS, default=0)
