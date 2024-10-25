40beeb8ad110e8fcf822e2a0b2976fa2bcc59b67
Jenna Guest <jenaleigh01@gmail.com>
2024-10-25 10:49:24 +0000

    generic post view

--- yoga_blog/urls.py
+++ yoga_blog/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home.views import index
-from blog.views import my_blog
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
-    path('blog', my_blog, name='blog'),
+    path('blog/', include("blog.urls"), name='blog'),
 ]
