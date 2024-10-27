a66d33338703fc12f827ee7dcce450283bf2a32d
Jenna Guest <jenaleigh01@gmail.com>
2024-10-27 02:30:05 +0000

    added post_detail view

--- yoga_blog/settings.py
+++ yoga_blog/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jenaleigh01-yogaattheba-rr9rhuin31z.ws-eu116.gitpod.io', '.herokuapp.com']
 
